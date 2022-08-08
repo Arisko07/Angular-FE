da540489e7139cb55ec349d8a42c85e014d1490d
AKO <arisv.ko@gmail.com>
2022-08-08 11:52:58 +0800

    Declare components and directive s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './header/navigation/navigation.component';
+import { BackgroundImageDirective } from './custom-directives/background-image.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    BackgroundImageDirective
   ],
   imports: [
     BrowserModule
