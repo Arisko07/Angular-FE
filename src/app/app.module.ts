829d5301d4c6897d486cc0da031715dab3beb6cb
AKO <arisv.ko@gmail.com>
2022-08-10 13:41:45 +0800

    Created footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HeaderComponent } from './header/header.component';
 import { NavigationComponent } from './header/navigation/navigation.component';
 import { BackgroundImageDirective } from './custom-directives/background-image.directive';
 import { NavigationService } from './custom-service/navigation.service';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavigationComponent,
-    BackgroundImageDirective
+    BackgroundImageDirective,
+    FooterComponent
   ],
   imports: [
     BrowserModule
