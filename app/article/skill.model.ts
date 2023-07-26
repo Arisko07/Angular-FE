74ff80ba1fcf693f90815afd0272676c811dc896
AKO <arisv.ko@gmail.com>
2023-07-26 22:22:23 +0800

    Auto-generated commit [ci skip]

--- app/article/skill.model.ts
+++
@@ -1,9 +0,0 @@
-export class Skill{
-
-    constructor(public icon: string, public name: string, public type: string, public description: string){
-        this.icon = icon;
-        this.name = name;
-        this.type = type;
-        this.description = description;
-    }
-}
